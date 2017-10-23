375bd0d940be0aba8252b32852d8e7f8b4ae5263
CristoJV <cristo.alu.ulpgc@gmail.com>
2017-10-23 20:25:06 +0100

    videoWebPlay v2 - Deployed database

--- videoWebPlay/routes/index.js
+++ videoWebPlay/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+//Postgresql database module
+var pg = require('pg');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/videos', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM videos_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/videos', {results: result.rows} ); }
+    });
+  });
 });
 
 module.exports = router;
